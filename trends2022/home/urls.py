08ceed71eb946e1bd6e5ea87c3c76dae43980b85
rajeev <rajeevmohan1127@gmail.com>
2022-08-30 14:36:51 +0530

    login and regiter completed

--- trends2022/home/urls.py
+++ trends2022/home/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.index, name="Homepage"),
     path('samp/',views.samp, name="Testpage"),
-    path('login1/',views.login1, name="Loginpage"),
-    path('register/',views.register, name="Registerpage"),
-    path('login1/logincheck/',views.logincheck, name="Logincheck"),
-    path('register/registercheck/',views.registercheck, name="Registercheck"),
-    ]
+    path('login1/',views.login1, name="login"),
+    path('register/',views.register, name="register"),
+        ]
--- trends2022/home/views.py
+++ trends2022/home/views.py
@@ -16,50 +16,47 @@ def samp(request):
 
 
 def login1(request):
-    return render(request,"login.html")
+    if request.method=='POST':
+        name=request.POST['uname']
+        password=request.POST['iname']
+        user=auth.authenticate(username=name,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            msg="Invalid username or Password"
+            return render(request,"login.html",{'msg':msg})
+    else:
+        return render(request,"login.html")
 
 
 def register(request):
-    return render(request,"register.html")
+    if request.method=='POST':
+        username=request.POST['username']
+        firstname=request.POST['firstname']
+        lastname=request.POST['lastname']
+        email=request.POST['email']
+        password=request.POST['password']
+        repassword=request.POST['repassword']
+        uchk=User.objects.filter(username=username)
+        echk=User.objects.filter(email=email)
+        print(uchk)
+        if uchk:
+            msg="Username is already taken"
+            return render(request,"register.html",{'msg':msg,})
+  
+        elif echk:
+            msg="Email is already taken"
+            return render(request,"register.html",{'msg':msg,})
     
-
-
-def logincheck(request):
-    name=request.GET['uname']
-    password=request.GET['iname']
-    user=auth.authenticate(username=name,password=password)
-    if user is not None:
-        auth.login(request,user)
-        return redirect('/')
+        elif password!=repassword:
+            msg="Invalid password"
+            return render(request,"register.html",{'msg':msg,})
+        
+        else:
+            user=User.objects.create_user(username=username,first_name=firstname,last_name=lastname,email=email,password=password)
+            user.save();
+            auth.login(request,user)
+            return redirect('/')
     else:
-        return redirect('/login1')
-
-def registercheck(request):
-    username=request.GET['username']
-    firstname=request.GET['firstname']
-    lastname=request.GET['lastname']
-    email=request.GET['email']
-    password=request.GET['password']
-    repassword=request.GET['repassword']
-    uchk=User.objects.filter(username=username)
-    echk=User.objects.filter(email=email)
-    print(uchk)
-    if uchk:
-        msg="Username is already taken"
-        return render(request,"test.html",{'key1':msg,})
-  
-    elif echk:
-        msg="Email is already taken"
-        return render(request,"test.html",{'key1':msg,})
-  
-    elif password!=repassword:
-        msg="Invalid password"
-        return render(request,"test.html",{'key1':msg,})
-    
-    else:
-        user=User.objects.create_user(username=username,first_name=firstname,last_name=lastname,email=email,password=password)
-        user.save();
-        return redirect('/')
-  
-    
-    
+        return render(request,"register.html")    

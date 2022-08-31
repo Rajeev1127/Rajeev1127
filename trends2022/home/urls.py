7f247509505635711e3d0a1dd648fb0e1b546ee0
rajeev <rajeevmohan1127@gmail.com>
2022-08-31 14:34:05 +0530

    Product row section

--- trends2022/home/urls.py
+++ trends2022/home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('samp/',views.samp, name="Testpage"),
     path('login1/',views.login1, name="login"),
     path('register/',views.register, name="register"),
+    path('logout/',views.logout, name="Logout")
         ]
